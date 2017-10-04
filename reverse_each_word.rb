e69ac5e901f9636d282f880bf319b5e011e3fbd5
Alicia Yu <alicia.yu.16@gmail.com>
2017-10-04 05:11:29 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,8 @@ def reverse_each_word(sentence)
   words = sentence.split(' ')
   reverse_sentence = []
   words.length.times do |i|
-    reverse_sentence[i] = words[i].reverse.collect
+    reverse_sentence[i] = words[i].reverse
   end
   reverse_sentence.join(" ")
+  reverse_sentence.collect
 end
