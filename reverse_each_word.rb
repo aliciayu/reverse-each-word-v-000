6e6e58fe56581679da5b7216df0efde6cf9d408e
Alicia Yu <alicia.yu.16@gmail.com>
2017-10-04 04:48:46 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
-def reverse_each_word(sentence)
-  sentence = sentence.new
+def reverse_each_word(sentence1)
+  sentence1 = sentence.new
   sentence.collect do |sentence|
     sentence.reverse
   end
